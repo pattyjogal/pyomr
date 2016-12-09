0cc57568e914837da7ab8819642ac8077a244cd4
Patrick Gallagher <patrickj@cpgallagher.com>
2016-12-09 15:18:02 -0600

    General improvements to the note recognition function of the shape model

--- pyomr/shape/shape.py
+++ pyomr/shape/shape.py
@@ -2,10 +2,12 @@ import copy
 
 import cv2
 
+import numpy as np
+
 from pyomr.img.colors import grayscale, binary_img
 
-def get_shapes_from_barline(img):
 
+def get_shapes_from_barline(img):
     """
     :param img: The input image (should be a barline for optimal results)
     :type img: numpy.ndarray
@@ -23,30 +25,109 @@ def get_shapes_from_barline(img):
     _, vert_size = vert.shape
 
     # Start by extracting the horizontal elements
-    print(horz_size)
-    h_kernel = cv2.getStructuringElement(
-        cv2.MORPH_RECT,
-        (int(horz_size / 4.33333), 1)
-    )
+    for i in range(1, 20):
+        print(horz_size)
+        h_kernel = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE,
+            (i, i)
+        )
 
-    h_img = cv2.morphologyEx(
-        img,
-        cv2.MORPH_OPEN,
-        h_kernel
-    )
+        h_img = cv2.morphologyEx(
+            img,
+            cv2.MORPH_OPEN,
+            h_kernel
+        )
 
-    # Continue by extracting the vertical elements
+        # Continue by extracting the vertical elements
 
-    v_kernel = cv2.getStructuringElement(
-        cv2.MORPH_RECT,
-        # TODO: Figure out why this has to be super low
-        (1, 2)
-    )
+        v_kernel = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE,
+            # TODO: Figure out why this has to be super low
+            (int(horz_size / 9), 4
 
-    v_img = cv2.morphologyEx(
-        img,
-        cv2.MORPH_OPEN,
-        v_kernel
-    )
+             )
+        )
 
-    return (~h_img, ~v_img)
+        v_img = cv2.morphologyEx(
+            img,
+            cv2.MORPH_OPEN,
+            v_kernel
+        )
+
+        # For now, the user can choose the appropriate step
+        cv2.imshow('Step {}'.format(i), h_img)
+        if cv2.waitKey(0) == 13:
+            cv2.destroyAllWindows()
+            print('Ideal image selected')
+            ret = h_img
+            break
+
+    return ~ret, ~v_img
+
+
+def locate_notes(pure_img):
+    """
+    Using the Hough Circles Transform functions of CV2, find and
+    mark all notes.
+    :param img: The input image (should be a barline for optimal results)
+    :type img: numpy.ndarray
+    :type img: str
+    :return:
+    """
+
+    if type(pure_img) is str:
+        pure_img = cv2.imread(pure_img)
+
+    note_width = specify_width(pure_img)
+
+    img = get_shapes_from_barline(pure_img)[0]
+    img = cv2.medianBlur(img, 3)
+    bimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    # Detect circles
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, note_width,
+                               param1=10, param2=8, minRadius=int(note_width / 2 - 10), maxRadius=int(note_width / 2 + 1))
+
+    try:
+        circles = np.uint16(np.around(circles))
+    except AttributeError:
+        circles = []
+    else:
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(pure_img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(pure_img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    cv2.namedWindow('detected circles', cv2.WINDOW_NORMAL)
+    cv2.imshow('detected circles', pure_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return pure_img
+
+
+def specify_width(img):
+    """
+
+    :param img: A numpy ndarray representing the target image
+    :returns: Width of note
+    """
+    coord = []
+
+    def on_mouse(event, x, y, flags, param):
+
+        nonlocal coord
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            coord.append(x)
+
+    cv2.namedWindow('Select the bound of a note', cv2.WINDOW_NORMAL)
+
+    cv2.setMouseCallback('Select the bound of a note', on_mouse)
+
+    while len(coord) is not 2:
+        cv2.imshow('Select the bound of a note', img)
+        cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
+    return abs(coord[1] - coord[0])
--- pyomr/test/shape/shape_test.py
+++ pyomr/test/shape/shape_test.py
@@ -5,10 +5,120 @@ import logging
 from pyomr.shape.shape import *
 from pyomr.img.filters import *
 
+import matplotlib.pyplot as plt
+from scipy.interpolate import spline, splev, splrep
+from scipy.signal import argrelextrema
+
 dir = os.path.dirname(__file__)
 print(dir)
 stock_img = os.path.join(dir, '../../../img/sheet_basic.png')
 good_q_camera = os.path.join(dir, '../../../img/good_q_camera.jpg')
+mild_music = os.path.join(dir, '../../../img/mild_music.jpg')
+mtttp = os.path.join(dir, '../../../img/mtttp.jpg')
+mtttp_flash = os.path.join(dir, '../../../img/flash.jpg')
+mtttp_crump = os.path.join(dir, '../../../img/crumpled.jpg')
+mtttp_vanilla = os.path.join(dir, '../../../img/vanilla.jpg')
+russ = os.path.join(dir, '../../../img/russ.jpg')
+gvb = os.path.join(dir, '../../../img/gvb.jpg')
+
+
+def test_kernel(src, name, numstep):
+
+    cv2.imwrite('output/{}.png'.format(name), locate_notes(src))
+
+    img = binary_img(~grayscale(src))
+    oimg = cv2.imread(src, 0)
+    # To get vertical vs horizontal, we need to clone:
+    horz = copy.copy(img)
+    vert = copy.copy(img)
+
+    horz_size, _ = horz.shape
+    _, vert_size = vert.shape
+
+    last_h_img = None
+
+    x_plot = np.array([0])
+    y_plot = np.array([0])
+    dpstep = 0
+    # Start by extracting the horizontal elements
+    for i in range(1, 20):
+        print(horz_size)
+        h_kernel = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE,
+            (i, i)
+        )
+
+        h_img = cv2.morphologyEx(
+            img,
+            cv2.MORPH_OPEN,
+            h_kernel
+        )
+
+        # Continue by extracting the vertical elements
+
+        v_kernel = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE,
+            # TODO: Figure out why this has to be super low
+            (int(horz_size / 9), 4
+
+             )
+        )
+
+        v_img = cv2.morphologyEx(
+            img,
+            cv2.MORPH_OPEN,
+            v_kernel
+        )
+        dp = 0
+        if last_h_img is not None:
+            dp = (np.count_nonzero(last_h_img)) - (np.count_nonzero(h_img))
+            print('{}: On trial {}, we lost {} px from the last image'.format(name,i, dp))
+        last_h_img = h_img
+
+        cv2.imwrite('kernel/' + name + '_' + str(i) + '_kernel_dp_{}.png'.format(dp), h_img)
+
+        if i is numstep:
+            dpstep = dp
+            good_img = h_img
+            cv2.imwrite('{}_ideal.png'.format(name), ~good_img)
+
+        x_plot = np.append(x_plot, i)
+        y_plot = np.append(y_plot, dp)
+
+    # Try and find a best fit curve
+    x_smooth = np.linspace(x_plot.min(), x_plot.max(), 300)
+    y_smooth = spline(x_plot, y_plot, x_smooth)
+    minm = argrelextrema(y_smooth, np.less)
+    print(minm)
+
+
+    plt.annotate('Accepted point', xy=(numstep, dpstep), xytext=(numstep + 3, dpstep + 100), arrowprops=dict(facecolor='black', shrink=0.05))
+    plt.plot(x_plot, y_plot)
+    plt.plot(x_smooth, y_smooth)
+    plt.suptitle('{}: Loss of Pixels per Step'.format(name), fontsize=14, fontweight='bold')
+    plt.draw()
+    plt.savefig('{}_trend.png'.format(name))
+    plt.clf()
+
+    # Show keypoints
+    pure_img = cv2.imread(src,)
+    img = cv2.medianBlur(~good_img, 3)
+    bimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    print('Testing Hough Circles')
+
+    # Detect circles
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 5,
+                               param1=10, param2=10, minRadius=1, maxRadius=20)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # draw the outer circle
+        cv2.circle(pure_img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv2.circle(pure_img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    cv2.imwrite('detected_circle_{}.png'.format(name), pure_img)
+    cv2.imshow('detected circles', pure_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def test_image_components(message, src):
     logging.info('Testing a ' + message)
@@ -34,7 +144,7 @@ def test_filtered_image_components(message, src):
     cv2.waitKey(3000)
     cv2.destroyWindow('vert')
 
-def test_recognition(lily_notes):
+def test_recognition(lily_notes, desc):
     import subprocess
     from PIL import Image
 
@@ -64,15 +174,23 @@ def test_recognition(lily_notes):
 
     img_path = dir_path + '\\' + path[:-2] + 'preview.png'
 
-    test_image_components('generated image', img_path)
+    test_kernel(img_path, 'lily_{}'.format(desc), 4)
+    # test_image_components('generated image', img_path)
 
 
-test_recognition(["b'", "c'", "a'", "g'"])
-test_recognition(["b'", "c'", "a'", "g'", "e'", "c''", "a'"])
-test_recognition(["b'", "c'", "a'", "g'", "e'", "c''", "a'"] + ["b'", "c'", "a'", "g'", "e'", "c''", "a'"] + ["b'", "c'", "a'", "g'", "e'", "c''", "a'"])
-
-test_image_components('stock image', stock_img)
-test_image_components('good quality camera image', good_q_camera)
+test_recognition(["b'", "c'", "a'", "g'"], 'small')
+test_recognition(["b'", "c'", "a'", "g'", "e'", "c''", "a'"], 'med')
+test_recognition(["b'", "c'", "a'", "g'", "e'", "c''", "a'"] + ["b'", "c'", "a'", "g'", "e'", "c''", "a'"] + ["b'", "c'", "a'", "g'", "e'", "c''", "a'"], 'large')
+test_kernel(stock_img, 'Scanned Music', 4)
+test_kernel(good_q_camera, 'Uneven Barline Music', 9)
+test_kernel(mild_music, 'PDF', 2)
+test_kernel(mtttp, 'MTTTP On a Computer Screen', 9)
+test_kernel(mtttp_crump, 'Crumpled up MTTTP', 9)
+test_kernel(mtttp_flash, 'Flash Photo of MTTTP', 9)
+test_kernel(mtttp_vanilla, 'Vanilla MTTTP', 9)
+test_kernel(russ, 'Russian', 2)
+test_kernel(gvb, 'Grande Valse Brillante', 3)
+# test_image_components('stock image', stock_img)
+# test_image_components('good quality camera image', good_q_camera)
 # test_filtered_image_components('stock image', stock_img)
 # test_filtered_image_components('good quality camera image', good_q_camera)
-
